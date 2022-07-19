62efc68f5219925d2b22fc6061f77114e984b561
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-07-19 01:40:40 -0400

    Create RockPaperScissors.rb
    Draft 101

---
+++ RockPaperScissors.rb
@@ -0,0 +1,51 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+	Kernel.puts("=> #{message}")
+end
+
+def test_method
+	prompt("=> testing")
+end
+
+test_method
+
+def display_resuls (player, computer)
+	if (player == 'rock' && computer == 'scissors') ||
+			(player == 'paper' && computer == 'rock' ) ||
+			(player == 'scissors' &&  computer == 'paper')
+		prompt("You won")
+	elsif (player == 'rock' && computer == 'paper') ||
+				(player == 'paper' && computer == 'scissors') ||
+				(player == 'scissors' && computer == 'rock')
+		prompt("Computer won!")
+	else
+		prompt("It's a tie!")
+	end
+end
+
+loop do
+	choice = ''
+	loop do
+		prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+		choice = Kernel.gets().chomp()
+
+		if VALID_CHOICES.include?(choice)
+			break
+		else
+			prompt("That's not a valid choice.")
+		end
+	end
+
+	computer_choice = VALID_CHOICES.sample
+
+	prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+	display_resuls(choice, computer_choice)
+
+	prompt("Do you want to play again?")
+	answer = Kernel.gets().chomp()
+	break unless answer.downcase().start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
