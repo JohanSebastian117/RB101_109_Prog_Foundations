247d465847cddac873b5193361987b25a4a7b626
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-07-21 03:04:31 -0400

    Update Rock_paper_scissors.rb

--- Rock_paper_scissors.rb
+++ Rock_paper_scissors.rb
@@ -1,22 +1,20 @@
-VALID_CHOICES = %w['rock' 'paper' 'scissors']
+VALID_CHOICES = %w[rock paper scissors]
 
 def prompt(message)
-	Kernel.puts("=> #{message}")
+  Kernel.puts("=> #{message}")
 end
 
 def test_method
-	prompt("=> testing 101")
+prompt("=> testing 101")
 end
-
-test_method
 
 def display_resuls (player, computer)
 	if (player == 'rock' && computer == 'scissors') ||
 			(player == 'paper' && computer == 'rock' ) ||
 			(player == 'scissors' &&  computer == 'paper')
 		prompt("You won")
-	elsif (player == 'rock' && computer == 'paper') ||
-				(player == 'paper' && computer == 'scissors') ||
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
 				(player == 'scissors' && computer == 'rock')
 		prompt("Computer won!")
 	else
@@ -28,26 +26,24 @@ loop do
 	choice = ''
 	loop do
 		prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-		choice = Kernel.gets().chomp()
+		choice = Kernel.gets.chomp
 
-		if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES.include?(choice)
 			break
-		else
-			prompt("That's not a valid choice.")
-		end
-	end
+    else
+    prompt("That's not a valid choice.")
+    end
+ end
 
-	computer_choice = VALID_CHOICES.sample
+ computer_choice = VALID_CHOICES.sample
 
-	prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+ prompt("You chose: #{choice}, Computer chose: #{computer_choice}")
 
-	display_resuls(choice, computer_choice)
+ display_resuls(choice, computer_choice)
 
-	prompt('Do you want to play again?')
-	answer = Kernel.gets().chomp()
-	break unless answer.downcase().start_with?('y')
+ prompt('Do you want to play again?')
+ answer = Kernel.gets.chomp
+ break unless answer.downcase.start_with?('y')
 end
 
-prompt("Thank you for playing. Good bye!")
-#test 101
-
+prompt('Thank you for playing. Good bye!')
