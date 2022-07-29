ed6c2aaab57794dd04ea785db6c8afdc156f8c19
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-07-29 15:22:20 -0400

    Re-install rubo cop & test new prog

--- Gemfile
+++ Gemfile
@@ -1,9 +1,5 @@
-# frozen_string_literal: true
-
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 group :development do
   gem 'rubocop', '0.86.0'
 end
-
-# gem "rails"
--- Rock_paper_scissors.rb
+++ Rock_paper_scissors.rb
@@ -1,55 +1,54 @@
 VALID_CHOICES = %w[rock paper scissors lizard spock]
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
 def win?(first, second)
- (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock' ) ||
-    (first == 'scissors' &&  second == 'paper') ||
-		(first == 'lizard' && second == 'spock') ||
-		(first == 'spock' && second == 'scissors') ||
-		(first == 'scissors' && second == 'lizard') ||
-		(first == 'lizard' && second == 'paper') ||
-		(first == 'spock' && second == 'rock') ||
-		(first == 'rock' && second == 'lizard') ||
-		(first == 'paper' && second == 'spock')
-
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'scissors' && second == 'paper') ||
+	  (first == 'lizard' && second == 'spock') ||
+	  (first == 'spock' && second == 'scissors') ||
+	  (first == 'scissors' && second == 'lizard') ||
+	  (first == 'lizard' && second == 'paper') ||
+	  (first == 'spock' && second == 'rock') ||
+	  (first == 'rock' && second == 'lizard') ||
+	  (first == 'paper' && second == 'spock')
 end
 
-def display_resuls (player, computer)
+def display_resuls(player, computer)
   if win?(player, computer)
-		prompt("You won")
+    prompt("You won")
   elsif win?(computer, player)
-		prompt("Computer won!")
-	else
-		prompt("It's a tie!")
-	end
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
 end
 
 loop do
-	choice = ''
-	loop do
-		prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-		choice = Kernel.gets.chomp
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp.downcase
 
     if VALID_CHOICES.include?(choice)
-			break
+      break
     else
-    prompt("That's not a valid choice.")
+      prompt("That's not a valid choice.")
     end
- end
+  end
 
- computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.sample
 
- prompt("You chose: #{choice}, Computer chose: #{computer_choice}")
+  prompt("You chose: #{choice}, Computer chose: #{computer_choice}")
 
- display_resuls(choice, computer_choice)
+  display_resuls(choice, computer_choice)
 
- prompt('Do you want to play again?')
- answer = Kernel.gets.chomp
- break unless answer.downcase.start_with?('y')
+  prompt('Do you want to play again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt('Thank you for playing. Good bye!')
