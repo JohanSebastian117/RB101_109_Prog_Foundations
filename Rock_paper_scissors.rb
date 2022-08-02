92c747c79e640d2fb877d2db8f4c7e082a94aac6
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-08-02 00:55:15 -0400

    Update Rock_paper_scissors.rb
    Working with odd score keeping

--- Rock_paper_scissors.rb
+++ Rock_paper_scissors.rb
@@ -28,7 +28,7 @@ def win?(first, second)
   win_conditions[first].include?(VALID_CHOICES[second])
 end
 
-def display_results(player, computer)
+def display_result(player, computer)
   if win?(player, computer)
     prompt("You won")
   elsif win?(computer, player)
@@ -37,30 +37,56 @@ def display_results(player, computer)
     prompt("It's a tie!")
   end
 end
+
+def display_grand_winner(player_wins, _)
+  if player_wins == 3
+    prompt("You are the grand winner!")
+  else
+    prompt("Computer is the grand winner!")
+  end
+end
+
 # Main loop
 loop do
-  choice = ''
-  loop do
-    prompt("Choose one: #{prompt_choices}")
-    choice = gets.chomp.downcase
+	player_total = 0
+	computer_total = 0
+	loop do
+		choice = ''
+		loop do
+			prompt("Choose one: #{prompt_choices}")
+			choice = gets.chomp.downcase
 
-    if VALID_CHOICES.include?(choice)
-      break
+			if VALID_CHOICES.include?(choice)
+				break
+			else
+				prompt("That's not a valid choice.")
+			end
+		end
+
+		computer_choice = VALID_CHOICES.keys.sample
+
+		prompt("You chose: #{VALID_CHOICES[choice]}")
+		prompt("Computer chose: #{VALID_CHOICES[computer_choice]}")
+
+		display_result(choice, computer_choice)
+
+		if win?(choice, computer_choice)
+      player_total += 1
+    elsif win?(computer_choice,choice)
+      computer_total += 1
     else
-      prompt("That's not a valid choice.")
+      player_total += 0 && computer_total += 0
     end
+
+		prompt("Your total: #{player_total}. Computer total: #{computer_total}.")
+
+    break if player_total == 3 || computer_total == 3
   end
-
-  computer_choice = VALID_CHOICES.keys.sample
-
-  prompt("You chose: #{VALID_CHOICES[choice]}")
-  prompt("Computer chose: #{VALID_CHOICES[computer_choice]}")
-
-  display_results(choice, computer_choice)
-
-  prompt('Do you want to play again?')
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+	
+	display_grand_winner(player_total, computer_total)
+	prompt('Do you want to play again?')
+		answer = gets.chomp
+		break unless answer.downcase.start_with?('y')
 end
 
 prompt('Thank you for playing. Good bye!')
