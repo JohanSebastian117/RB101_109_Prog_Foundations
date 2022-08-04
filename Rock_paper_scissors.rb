07d1385f528e5722269669e3e9a1953562695b81
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-08-03 23:40:38 -0400

    Update Rock_paper_scissors.rb
    3 rounds added, issues with scoring (a tie equals a point for both player and computer)

--- Rock_paper_scissors.rb
+++ Rock_paper_scissors.rb
@@ -6,6 +6,8 @@ end
 prompt("Welcome to Rock Paper Scissors 2.0")
 prompt("best of 3 wins!!!")
 
+# Constants
+
 VALID_CHOICES = {
   'r' => 'rock',
   'p' => 'paper',
@@ -22,12 +24,14 @@ prompt_choices = "
 	sp = spock"
 
 def win?(first, second)
-  win_conditions = {
-    'r' => ['scissors', 'lizard'],
-    'p' => ['rock', 'spock'],
-    'sc' => ['paper', 'lizard'],
-    'sp' => ['scissors', 'rock'],
-    'l' => ['paper', 'spock'] }
+  win_conditions =
+    {
+      'r' => ['scissors', 'lizard'],
+      'p' => ['rock', 'spock'],
+      'sc' => ['paper', 'lizard'],
+      'sp' => ['scissors', 'rock'],
+      'l' => ['paper', 'spock']
+    }
   (win_conditions[first].include?(VALID_CHOICES[second]))
 end
 
@@ -50,46 +54,47 @@ def display_grand_winner(player_wins, _)
 end
 
 # Main loop
+
 loop do
-	player_total = 0
-	computer_total = 0
-	loop do
-		choice = ''
-		loop do
-			prompt("Choose one: #{prompt_choices}")
-			choice = gets.chomp.downcase
+  player_total = 0
+  computer_total = 0
+  loop do
+    choice = ''
+    loop do
+      prompt("Choose one: #{prompt_choices}")
+      choice = gets.chomp.downcase
 
-			if VALID_CHOICES.include?(choice)
-				break
-			else
-				prompt("That's not a valid choice.")
-			end
-		end
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        prompt("That's not a valid choice.")
+      end
+    end
 
-		computer_choice = VALID_CHOICES.keys.sample
+    computer_choice = VALID_CHOICES.keys.sample
 
-		prompt("You chose: #{VALID_CHOICES[choice]}")
-		prompt("Computer chose: #{VALID_CHOICES[computer_choice]}")
+    prompt("You chose: #{VALID_CHOICES[choice]}")
+    prompt("Computer chose: #{VALID_CHOICES[computer_choice]}")
 
-		display_result(choice, computer_choice)
+    display_result(choice, computer_choice)
 
-		if win?(choice, computer_choice)
+    if win?(choice, computer_choice)
       player_total += 1
-    elsif win?(computer_choice,choice)
+    elsif win?(computer_choice, choice)
       computer_total += 1
     else
       player_total += 0 && computer_total += 0
     end
 
-		prompt("Your total: #{player_total}. Computer total: #{computer_total}.")
+    prompt("Your total: #{player_total}. Computer total: #{computer_total}.")
 
     break if player_total == 3 || computer_total == 3
   end
 
-	display_grand_winner(player_total, computer_total)
-	prompt('Do you want to play again?')
-		answer = gets.chomp
-		break unless answer.downcase.start_with?('y')
+  display_grand_winner(player_total, computer_total)
+  prompt('Do you want to play again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt('Thank you for playing. Good bye!')
