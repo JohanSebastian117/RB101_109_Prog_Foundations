d81b6908a029629f12aec9f05711d464db2b2e50
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-08-29 22:38:27 -0400

    Update tip_calculator.rb

---
+++ Lesson 1/tip_calculator.rb
@@ -0,0 +1,13 @@
+puts '==> What is the bill?'
+bill = gets.to_f
+
+puts '==> What is the tip percentage?'
+tip_percent = gets.to_f / 100
+
+tip_total = (bill * tip_percent)
+bill_total = (bill + tip_total)
+
+puts "The tip is $#{tip_total}"
+puts "The total is $#{bill_total}"
+
+
