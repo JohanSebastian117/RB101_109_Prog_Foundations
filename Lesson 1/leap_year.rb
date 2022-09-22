54d91d7f91edba067b6af1c7bbcd369d83e98cf4
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-09-21 22:32:44 -0400

    Create leap_year.rb

---
+++ Lesson 1/leap_year.rb
@@ -0,0 +1,28 @@
+def leap_year?(year)
+	
+	if year % 4 == 0 && year % 100 != 0
+		puts true
+	elsif year % 400 == 0
+		puts true
+	elsif year % 4 == 0 && year % 100 == 0 && year % 400 != 0
+		puts false
+	elsif year % 4 != 0
+		puts false
+	end
+end
+
+
+
+leap_year?(2016) 
+leap_year?(2015) 
+leap_year?(2100) 
+leap_year?(2400) 
+leap_year?(240000)
+leap_year?(240001)
+leap_year?(2000)
+leap_year?(1900)
+leap_year?(1752)
+leap_year?(1700)
+leap_year?(1)
+leap_year?(100)
+leap_year?(400)
