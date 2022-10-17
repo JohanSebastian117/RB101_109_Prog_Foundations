6c02889be25583d3e242c78573c8236c4566cb0a
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2022-10-16 21:05:36 -0400

    Small problems folder added

--- Lesson 3/hard_1.rb
+++ Lesson 3/hard_1.rb
@@ -49,7 +49,7 @@ def dot_separated_ip_address?(input_string)
 
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop
-    return false unless is_an_ip_number?(word)
+    break unless is_an_ip_number?(word)
   end
   return true
 end
