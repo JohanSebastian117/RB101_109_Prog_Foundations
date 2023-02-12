d67098d1fad74391c4976c9051cbbdd3df105d2d
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2023-02-11 22:00:58 -0500

    go go

--- Small_problems/Easy_problems/after_midnight.rb
+++ Small_problems/Easy_problems/after_midnight.rb
@@ -6,32 +6,31 @@ MINUTES_PER_HOUR = 60
 HOURS_PER_DAY = 24
 MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
-# def reify_minutes_zero_to_1439(minutes)
-# 	while minutes < 0
-# 		minutes += MINUTES_PER_DAY
-# 	end
+def normalize_minutes_zero_to_1439(minutes)
+	while minutes < 0
+		minutes += MINUTES_PER_DAY
+	end
 
-# 	minutes % MINUTES_PER_DAY
-# end
+	minutes % MINUTES_PER_DAY
+end
 
-# def time_of_day(delta_minutes)
-# 	delta_minutes = reify_minutes_zero_to_1439(delta_minutes)
-# 	hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
-# 	format('%02d:%02d', hours, minutes)
-# end
+def time_of_day(delta_minutes)
+	delta_minutes = normalize_minutes_zero_to_1439(delta_minutes)
+	hours, minutes = delta_minutes.divmod(MINUTES_PER_HOUR)
+	format('%02d:%02d', hours, minutes)
+end
+
+p 1437.divmod(MINUTES_PER_HOUR)
 
 
-value = -3 += MINUTES_PER_DAY
 
-p value
-
-# p time_of_day(0) == "00:00"
-# p time_of_day(-3) == "23:57"
-# p time_of_day(35) == "00:35"
-# p time_of_day(-1437) == "00:03"
-# p time_of_day(3000) == "02:00"
-# p time_of_day(800) == "13:20"
-# p time_of_day(-4231) == "01:29"
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
 
 
 
