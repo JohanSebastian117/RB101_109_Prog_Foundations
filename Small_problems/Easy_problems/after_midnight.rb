faef291fe439ce48125c3f9d44b462b7e73a332b
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2023-02-19 15:30:08 -0500

    now

--- Small_problems/Easy_problems/after_midnight.rb
+++ Small_problems/Easy_problems/after_midnight.rb
@@ -72,35 +72,34 @@ Here the delta minutes are d
 
 
 
+
+
+
+
+
 #--------------Part 2
 
 # Write two methods that each take a time of day in 24 hour format,
-# and return the number of minutes before and after midnight, respectively.
+# and return the time of minutes before and after midnight, respectively.
 # Both methods should return a value in the range 0..1439.
 
 # You may not use ruby's Date and Time methods.
 
-MINUTES_PER_HOUR = 60
 HOURS_PER_DAY = 24
+MINUTES_PER_HOUR = 60
 MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR
 
-def normalize_minutes_zero_to_1439(minutes)
-	while minutes < 0
-		minutes += MINUTES_PER_DAY
-	end
-
-	minutes % MINUTES_PER_DAY
+def after_midnight(time)
+	hours, minutes = time.split(":").map(&:to_i)
+	(hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY
 end
 
-
-def after_midnight(number)
-
+def before_midnight(time)
+	delta_minutes = MINUTES_PER_DAY - after_midnight(time)
+	delta_minutes = 0 if delta_minutes == MINUTES_PER_DAY
+	delta_minutes
 end
 
-
-def before_midnight(number)
-
-end
 
 
 
@@ -110,3 +109,40 @@ p after_midnight('12:34') == 754
 p before_midnight('12:34') == 686
 p after_midnight('24:00') == 0
 p before_midnight('24:00') == 0
+
+
+=begin
+
+P: Understanding the Problem
+E: Examples and test cases
+D: Data Structures
+A: Algorithms
+C: Implementing a solution in Code
+
+------------ P
+
+
+# Write two methods that each take a time of day in 24 hour format,
+# and return the time of minutes before and after midnight, respectively.
+# Both methods should return a value in the range 0..1439.
+
+- input: We will be taking a string in time format 00:00
+- output: An integer representing minutes before or after midnight
+DRAFT OF ALGO
+
+
+
+-------------- E
+	examples above
+
+
+-------------- D
+
+
+
+
+
+
+=end
+
+
