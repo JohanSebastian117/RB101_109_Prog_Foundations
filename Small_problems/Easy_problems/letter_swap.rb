2828a16249bf9270af235ea9650d45cb04a8a154
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2023-02-23 19:58:34 -0500

    Feb 23

--- Small_problems/Easy_problems/letter_swap.rb
+++ Small_problems/Easy_problems/letter_swap.rb
@@ -20,22 +20,49 @@ A: Algorithms
 C: Implementing a solution in Code
 
 Algo - 
-	- We will take the STRING and seperate each word with the #split function
+1st Method - swap
 	- We will take these words and change the first and last letter 
-		- A loop might work here
+		- Take the string and have it go through = 
+			- #split to seperate each characther into words
+				- #map to set up a BLOCK with #chars (now each word is an array made up of seperated letters)
+						-  place 2nd method here
+
+2nd Method - swap_first_with_last
+- We take the first and last index of a string and switch them
+	- this method will be a component in the first method
+
+						- end the block
+							- take the word value, and #join it with a space between words
+							- Voila!
+
+
+
+
 	- We will take this ARRAY of words and #join them with then #join function into a string
 	- We will post this STRING as the result
 
 =end
 
-def swap(string)
-	string = string.split
-	string.map { |word| word.chars.rotate.join }.join(" ")
 
+
+
+def swap(word)
+	word = word.split.map do |string| string.chars
+		swap_first_with_last(string)
+	end
+	word.join(" ")
 end
 
 
 
+
+def swap_first_with_last(string)
+	string[0], string[-1] = string[-1], string[0]
+  string
+end
+
+
+p swap("hello we are testing")
 p swap('Oh what a wonderful day it is')
 
 
