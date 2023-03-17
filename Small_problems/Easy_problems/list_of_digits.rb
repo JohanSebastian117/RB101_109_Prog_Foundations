647555d2f284bed06afbe2241c76906c01073e90
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2023-03-17 15:44:10 -0400

    new

--- Small_problems/Easy_problems/list_of_digits.rb
+++ Small_problems/Easy_problems/list_of_digits.rb
@@ -4,15 +4,20 @@ and returns a list of the digits in the number.
 
 =end
 
+#Integer to String #to_s
+#Chars will make the string to array #chars
+#Numeric strings are turned to Integers // #map iterates through the array and turns the String to Integers
+
 def digit_list(numbers)
-	numbers = numbers.to_s.split(" ")
-	numbers.map(&:to_i)
+	numbers = numbers.to_s.chars.map(&:to_i)
+	p numbers
 end
  
 
 
-puts digit_list(12345)
 puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
 puts digit_list(7) == [7]                     # => true
 puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
 puts digit_list(444) == [4, 4, 4]             # => true
+
+
