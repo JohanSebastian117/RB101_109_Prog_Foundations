3ec9c90b0c5af1d240035903284a56c31e0b934b
JohanSebastian117 <100800657+JohanSebastian117@users.noreply.github.com>
2023-03-24 23:53:04 -0400

    new

--- Small_problems/Easy_problems/delete_vowels.rb
+++ Small_problems/Easy_problems/delete_vowels.rb
@@ -3,6 +3,11 @@ Write a method that takes an array of strings, and returns an array of the same 
 except with the vowels (a, e, i, o, u) removed.
 =end
 
+# using #map we iterate through the string
+# using #map we also use #delete to take away all the vowels
+# success
+
+
 def remove_vowels(string)
 	string.map { |string| string.delete('AEIOUaeiou')}
 end
@@ -12,3 +17,8 @@ end
 p remove_vowels(%w(abcdefghijklmnopqrstuvwxyz)) == %w(bcdfghjklmnpqrstvwxyz)
 p remove_vowels(%w(green YELLOW black white)) == %w(grn YLLW blck wht)
 p remove_vowels(%w(ABC AEIOU XYZ)) == ['BC', '', 'XYZ']
+
+
+# %w(foo bar) is a shortcut for ["foo", "bar"] .
+# Meaning it's a notation to write an array of strings 
+# separated by spaces instead of commas and without quotes around them
